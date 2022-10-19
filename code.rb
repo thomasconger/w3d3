31e73e737aa2478a407dc249a0567086283af262
Thomas Conger <t.e.conger@gmail.com>
2022-10-19 11:04:24 -0700

    solved rec_1

--- code.rb
+++ code.rb
@@ -1,1 +1,39 @@
-#this is a comment
+# Warmup
+# Write a recursive method, range, that takes a start and an end and returns an array of all numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
+# Write both a recursive and iterative version of sum of an array.
+
+def range(start,finish)
+  return [] if finish <= start
+
+  [start] + range(start+1, finish)
+
+end
+
+
+# Exponentiation
+# Write two versions of exponent that use two different recursions:
+
+# # this is math, not Ruby methods.
+
+# # recursion 1
+# exp(b, 0) = 1
+# exp(b, n) = b * exp(b, n - 1)
+
+# # recursion 2
+# exp(b, 0) = 1
+# exp(b, 1) = b
+# exp(b, n) = exp(b, n / 2) ** 2             [for even n]
+# exp(b, n) = b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
+
+def rec_1(base,power)
+  return 1 if power == 0
+
+  base * rec_1(base, power-1)
+
+end
+
+puts rec_1(0,-2)
+
+#3, 3
+
+# (3,3)
